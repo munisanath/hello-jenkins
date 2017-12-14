ba6ce1b4287dfe1c445d8920d3d46e25765ae237
munisanath <shodamgula.sanath@gmail.com>
2017-12-14 14:57:07 +0000

    changed content World

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello Jenkins', function(done) {
-    request(app).get('/').expect('hello Jenkins', done);
+  it('respond with hello Jenkins World', function(done) {
+    request(app).get('/').expect('hello Jenkins World', done);
   });
 });
