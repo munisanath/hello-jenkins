db3e1b06e1769d5dfd6e326f6d0f2a90b79bf15a
munisanath <shodamgula.sanath@gmail.com>
2017-12-15 10:30:46 +0000

    Add

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello Jenkins World', function(done) {
-    request(app).get('/').expect('hello Jenkins World', done);
+  it('respond with Iam using DevOps', function(done) {
+    request(app).get('/').expect('Iam using DevOps', done);
   });
 });
